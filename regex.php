fdce89ed11c61743f2c50905394220388694bb42
Ashish Kumar Mandal <666kmandal@gmail.com>
2021-03-06 00:52:37 +0530

    update

--- regex.php
+++ regex.php
@@ -1,5 +1,8 @@
 <?php
-	$pattern = "/^[a-zA-Z._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/";
+	// $valid_email = "/^[a-zA-Z._]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/";
+	// $valid_url = "/^(https:\/\/www\.|http:\/\/www\.|www\.|)[a-zA-Z0-9\-_$]+\.[a-zA-Z]{2,}$/";
+
+	$pattern = "/^($^*+){}[]?$/";
 	if(isset($_POST['submit'])){
 		$res = preg_match($pattern, $_POST['val']);
 		if($res){
@@ -19,16 +22,26 @@
 <pre>	
 
 //		write any Regular Expration code inside forward slash
+
 ^		Start
 $		End
+
 []		Rules that are applied to a single character
-+		Apply the rule to everything
++		One or more times
+*		Zero or more times
+?		Once or none
+{3}		Exactly three times
+{3,5}	Three to five times
+{3,}	Three or more times
+
 \s  	Allow whitespace or spaces
 \d  	Allow all digits
 \w  	Allow alphanumeric charecters and underscore
-.		Allow all characters and symboles
 \.		Period
-{5}		Apply rule to the next 5 charecter
-{2,5}	Apply rule to between 2 and 5 character
+.		Allow all characters and symboles
+
+
+()		Rules that are applied for all characters
+|		Logical OR operator
 
 </pre>
